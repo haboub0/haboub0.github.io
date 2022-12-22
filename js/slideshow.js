cdd0daf821cb5d19f1683a3b9e8fcea497ff19a3
haboub0 <raphaelkattoura18@gmail.com>
2022-12-21 18:05:47 -0600

    first commit

--- js/slideshow.js
+++ js/slideshow.js
@@ -2,7 +2,7 @@ var slideIndex = 0
 var slides = document.querySelectorAll(".slideshow img")
 for (let slide of slides) slide.classList.add("slide", "fading")
 slideshow()
-function slideshow() {
+function slideshow() { 
     for (let slide of slides) slide.style.display = "none"
     slideIndex++
     if (slideIndex > slides.length) slideIndex = 1
